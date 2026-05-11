b0edf5a822188148e0fb3069182c4785f749327f
Thekiborg <doflr415@gmail.com>
2026-05-11 22:37:51 +0200

    todo list change

--- todo.gd
+++ todo.gd
@@ -1,21 +1,21 @@
 extends Node
 
 """
-Hacer logo
-Implementar ventana de inventario a abrir con TAB.
-- Ver objetos en una galeria
-- Hacer click para equipar o tirar
-Implementar item agarrable en el suelo cuando estes cerca con el espacio.
-- Tendran billboard sprite
-Implementar esquive con iframes en espacio y block con iframes en arma.
-Implementar barras de cargas del lo kenrai.
-- Bloquear algunos usos de M2 a coste de carga
-- Hacer que M1 y M2 produzcan cargas
-Hacer espada, lanza y halabarda.
-Hacer enemigo
-- IA, Arma, Textura
-Zona de tutorial
-Zona de demo
-- Enemigos. Mínimo 1 normal y 1 grande (boss)
-- Armas y objetos por ahi para lootear
+1. Hacer logo
+2. Implementar ventana de inventario a abrir con TAB.
+	- Ver objetos en una galeria
+	- Hacer click para equipar o tirar
+3. Implementar item agarrable en el suelo cuando estes cerca con el espacio.
+	- Tendran billboard sprite
+4. Implementar esquive con iframes en espacio y block con iframes en arma.
+5. Implementar barras de cargas del lo kenrai.
+	- Bloquear algunos usos de M2 a coste de carga
+	- Hacer que M1 y M2 produzcan cargas
+6. Hacer espada, lanza y halabarda.
+7. Hacer enemigo
+	- IA, Arma, Textura
+8. Zona de tutorial
+9. Zona de demo
+	- Enemigos. Mínimo 1 normal y 1 grande (boss)
+	- Armas y objetos por ahi para lootear
 """
